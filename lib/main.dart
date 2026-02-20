8321eb9c1b448aa25c57b4018086df165ec883af
wry <692135610@qq.com>
2026-02-20 09:10:34 +0800

    feat: Implement setup and host screens with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
+import 'screens/setup_screen.dart' as setup;
+import 'screens/host_screen.dart' as host;
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '谁是卧底助手',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => setup.SetupScreen(),
+        '/host': (context) => host.HostScreen(),
+        // 后续可以添加 '/vote' 等
+      },
+    );
+  }
+}
